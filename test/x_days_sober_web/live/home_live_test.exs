d5c98bb3ba403567a3609373e360eb472a0bf945
Sebastian Göttschkes <sebastian@agileaddicts.com>
2022-12-19 17:31:20 +0100

    Chore: Remove design and Phoenix texts

--- test/x_days_sober_web/live/home_live_test.exs
+++ test/x_days_sober_web/live/home_live_test.exs
@@ -3,6 +3,6 @@ defmodule XDaysSoberWeb.HomeLiveTest do
 
   test "GET /", %{conn: conn} do
     conn = get(conn, "/")
-    assert html_response(conn, 200) =~ "Welcome to Phoenix!"
+    assert html_response(conn, 200) =~ "X Days Sober"
   end
 end
