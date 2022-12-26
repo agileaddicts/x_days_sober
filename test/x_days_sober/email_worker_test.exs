f501591bba3b916f1ac8338ce3f1bd0079e4b8b8
Sebastian Göttschkes <sebastian@agileaddicts.com>
2022-12-26 09:18:26 +0100

    Chore: Add tests for email worker

---
+++ test/x_days_sober/email_worker_test.exs
@@ -0,0 +1,36 @@
+defmodule XDaysSober.EmailWorkerTest do
+  use XDaysSober.DataCase, async: true
+  use Oban.Testing, repo: XDaysSober.Repo
+
+  import Swoosh.TestAssertions
+  import XDaysSober.Factory
+
+  alias XDaysSober.EmailWorker
+
+  describe "perform/1" do
+    test "executes job but does not send any mails with no persons in database" do
+      assert perform_job(EmailWorker, %{}) == :ok
+      assert_no_email_sent()
+    end
+
+    test "executes job but does not send any mails with person signed up today" do
+      insert!(:person)
+
+      assert perform_job(EmailWorker, %{}) == :ok
+      assert_no_email_sent()
+    end
+
+    test "executes job and does send one email to person" do
+      insert!(:person,
+        sober_since:
+          "Europe/Vienna"
+          |> Timex.now()
+          |> Timex.subtract(Timex.Duration.from_days(1))
+          |> Timex.to_date()
+      )
+
+      assert perform_job(EmailWorker, %{}) == :ok
+      assert_email_sent()
+    end
+  end
+end
