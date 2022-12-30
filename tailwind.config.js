0e7335b1da573d0928f9fd2a95c098194c52410a
Sebastian Göttschkes <sebastian@agileaddicts.com>
2022-12-30 20:12:45 +0100

    Fix: Fix parcel and tailwind integration

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,14 +1,15 @@
+/** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./assets/js/**/*.js", "./lib/*_web.ex", "./lib/*_web/**/*.*ex"],
-  darkMode: "class",
   theme: {
+    extend: {},
     colors: {
       'ruby': '#D81E5B',
       'prussian-blue': '#102E4A',
       'cadet-blue': '#5AB1BB',
       'canary': '#FFFD98',
       'floral-white': '#FFF8F0'
-    }
+    },
   },
   plugins: [],
-};
+}
