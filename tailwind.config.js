1b660a7a9d19e44d6bc02f7f1d39fba4f8a148c5
Sebastian Göttschkes <sebastian@agileaddicts.com>
2023-01-01 21:48:47 +0100

    Feat: Change color in colorschema and add tailwind forms component

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,15 +1,22 @@
+const colors = require('tailwindcss/colors')
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./assets/js/**/*.js", "./lib/*_web.ex", "./lib/*_web/**/*.*ex"],
   theme: {
     extend: {},
     colors: {
-      'ruby': '#D81E5B',
+      transparent: 'transparent',
+      current: 'currentColor',
+      slate: colors.slate,
+      ruby: '#D81E5B',
       'prussian-blue': '#102E4A',
       'pine-green': '#157A6E',
-      'canary': '#FFFD98',
+      canary: '#FFFD98',
       'floral-white': '#FFF8F0'
     },
   },
-  plugins: [],
+  plugins: [
+    require('@tailwindcss/forms'),
+  ],
 }
