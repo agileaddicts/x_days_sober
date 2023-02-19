c01a325c1c711736fd7453aa1916a88ce437fff3
Sebastian Göttschkes <sebastian@agileaddicts.com>
2023-02-19 20:36:16 +0100

    Chore: Refactor calculation of subject and body for email once more

--- lib/x_days_sober/daily_email.ex
+++ lib/x_days_sober/daily_email.ex
@@ -11,6 +11,40 @@ defmodule XDaysSober.DailyEmail do
 
   require Logger
 
+  @subjects %{
+    years: %{
+      1 => "One year sober 🚀🚀🚀"
+    },
+    months: %{
+      1 => "One month sober 💥"
+    },
+    weeks: %{
+      1 => "One week sober 🎆"
+    },
+    days: %{
+      1 => "One day sober 🎉"
+    }
+  }
+
+  @bodys %{
+    years: %{
+      1 =>
+        "you did it! You are 1 year sober! Keep up the hard work and dedication to your sobriety, you are making positive changes in your life and deserve to be proud of yourself."
+    },
+    months: %{
+      1 =>
+        "I am proud of you for taking control of your recovery and making the choice to stay sober for the past month. This is a significant accomplishment, and you deserve to feel good about it!"
+    },
+    weeks: %{
+      1 =>
+        "Congratulations on one week of sobriety! You have already made a significant and positive change in your life, and should be proud of yourself for taking this important step. Remember that every day sober is a victory, and keep up the hard work. You are capable of living a fulfilling and healthy life without alcohol, and every day of sobriety brings you closer to achieving your goals. Keep going, and don't be afraid to reach out for support when you need it."
+    },
+    days: %{
+      1 =>
+        "Congratulations on taking the first step towards a sober lifestyle! The decision to get sober is not an easy one, and it takes a lot of courage and determination to make this change. Remember that you are not alone in this journey, and there are many resources and support systems available to help you along the way. Take it one day at a time, and be kind to yourself as you adjust to this new way of living. You are strong and capable, and you can do this. Keep up the good work!"
+    }
+  }
+
   def generate(person, sober_days) do
     name = person.name || person.email
 
@@ -69,45 +103,53 @@ defmodule XDaysSober.DailyEmail do
     )
   end
 
-  defp subject_text(%{years: years}) when years == 1, do: "One year sober 🚀🚀🚀"
-  defp subject_text(%{years: years}) when years > 0, do: "#{years} years sober 🚀🚀🚀"
+  defp subject_text(%{years: years}) when years > 0 do
+    Map.get(@subjects.years, years, "#{years} years sober 🚀🚀🚀")
+  end
 
-  defp subject_text(%{months: months}) when months == 1, do: "One month sober 💥"
-  defp subject_text(%{months: months}) when months > 0, do: "#{months} months sober 💥"
+  defp subject_text(%{months: months}) when months > 0 do
+    Map.get(@subjects.months, months, "#{months} months sober 💥")
+  end
 
-  defp subject_text(%{weeks: weeks}) when weeks == 1, do: "One week sober 🎆"
-  defp subject_text(%{weeks: weeks}) when weeks == 2, do: "Two weeks sober 🎆"
-  defp subject_text(%{weeks: weeks}) when weeks == 3, do: "Three weeks sober 🎆"
-  defp subject_text(%{weeks: weeks}) when weeks == 4, do: "Four weeks sober 🎆"
+  defp subject_text(%{weeks: weeks}) when weeks > 0 do
+    Map.get(@subjects.weeks, weeks, "#{weeks} weeks sober 🎆")
+  end
 
-  defp subject_text(%{weeks: weeks}) when weeks > 0, do: "#{weeks} weeks sober 🎆"
+  defp subject_text(%{days: days}) do
+    Map.get(@subjects.days, days, "#{days} days sober 🎉")
+  end
 
-  defp subject_text(%{days: days}) when days == 1, do: "One day sober 🎉"
-  defp subject_text(%{days: days}), do: "#{days} days sober 🎉"
+  defp text_body_text(%{years: years}) when years > 0 do
+    Map.get(
+      @bodys.years,
+      years,
+      "you did it! You are #{years} years sober! Keep up the hard work and dedication to your sobriety, you are making positive changes in your life and deserve to be proud of yourself."
+    )
+  end
 
-  defp text_body_text(%{weeks: weeks}) when weeks == 1,
-    do:
-      "Congratulations on one week of sobriety! You have already made a significant and positive change in your life, and should be proud of yourself for taking this important step. Remember that every day sober is a victory, and keep up the hard work. You are capable of living a fulfilling and healthy life without alcohol, and every day of sobriety brings you closer to achieving your goals. Keep going, and don't be afraid to reach out for support when you need it."
+  defp text_body_text(%{months: months}) when months > 0 do
+    Map.get(
+      @bodys.months,
+      months,
+      "you did it! You are #{months} months sober! Keep up the hard work and dedication to your sobriety, you are making positive changes in your life and deserve to be proud of yourself."
+    )
+  end
 
-  defp text_body_text(%{weeks: weeks}) when weeks == 2,
-    do:
-      "I am proud of you for taking control of your recovery and making the choice to stay sober for the past two weeks. This is a significant accomplishment, and you deserve to feel good about it!"
+  defp text_body_text(%{weeks: weeks}) when weeks > 0 do
+    Map.get(
+      @bodys.weeks,
+      weeks,
+      "you did it! You are #{weeks} weeks sober! Keep up the hard work and dedication to your sobriety, you are making positive changes in your life and deserve to be proud of yourself."
+    )
+  end
 
-  defp text_body_text(%{weeks: weeks}) when weeks == 3,
-    do:
-      "Wow! You are 3 weeks sober, that's awesome. One tip for someone who is abstinent from alcohol is to find a support system. It can be helpful to surround yourself with people who are also in recovery or who support your decision to live a sober lifestyle. This can include friends, family members, a therapist, or a support group such as Alcoholics Anonymous. Having people to talk to and share your experiences with can provide motivation and accountability as you continue on your journey of recovery."
-
-  defp text_body_text(%{weeks: weeks}) when weeks == 4,
-    do:
-      "You did not drink any alcohol for a full 4 weeks. I got a tip to keep up the good work: It can be helpful to have a plan in place for how to handle cravings or difficult situations that may arise. This could involve finding healthy ways to cope with stress, such as exercising or practicing relaxation techniques, or having a list of phone numbers to call when you need support. Remember that recovery is a process, and it's important to be patient and kind to yourself as you continue to learn and grow."
-
-  defp text_body_text(%{days: days}) when days == 1,
-    do:
-      "Congratulations on taking the first step towards a sober lifestyle! The decision to get sober is not an easy one, and it takes a lot of courage and determination to make this change. Remember that you are not alone in this journey, and there are many resources and support systems available to help you along the way. Take it one day at a time, and be kind to yourself as you adjust to this new way of living. You are strong and capable, and you can do this. Keep up the good work!"
-
-  defp text_body_text(%{days: days}),
-    do:
+  defp text_body_text(%{days: days}) do
+    Map.get(
+      @bodys.days,
+      days,
       "you did it! You are #{days} days sober! Keep up the hard work and dedication to your sobriety, you are making positive changes in your life and deserve to be proud of yourself."
+    )
+  end
 
   defp build_url(path) do
     base_url = Application.fetch_env!(:x_days_sober, :base_url)
--- test/x_days_sober/daily_email_test.exs
+++ test/x_days_sober/daily_email_test.exs
@@ -31,7 +31,7 @@ defmodule XDaysSober.DailyEmailTest do
                "you did it! You are 3 days sober! Keep up the hard work and dedication to your sobriety, you are making positive changes in your life and deserve to be proud of yourself."
     end
 
-    test "correct email for person on day 7" do
+    test "correct email for person after one week" do
       person = build_person_with_days_sober(%{}, 7)
       sober_days = Person.calculate_sober_days(person)
 
@@ -43,7 +43,19 @@ defmodule XDaysSober.DailyEmailTest do
                "Congratulations on one week of sobriety! You have already made a significant and positive change in your life, and should be proud of yourself for taking this important step. Remember that every day sober is a victory, and keep up the hard work. You are capable of living a fulfilling and healthy life without alcohol, and every day of sobriety brings you closer to achieving your goals. Keep going, and don't be afraid to reach out for support when you need it."
     end
 
-    test "correct email for person on after one month" do
+    test "correct email for person after three weeks" do
+      person = build_person_with_days_sober(%{}, 21)
+      sober_days = Person.calculate_sober_days(person)
+
+      email = DailyEmail.generate(person, sober_days)
+
+      assert email.subject == "3 weeks sober 🎆"
+
+      assert email.text_body =~
+               "you did it! You are 3 weeks sober! Keep up the hard work and dedication to your sobriety, you are making positive changes in your life and deserve to be proud of yourself."
+    end
+
+    test "correct email for person after one month" do
       one_month_before = Timex.now() |> Timex.shift(months: -1) |> Timex.to_date()
       person = build(:person, sober_since: one_month_before)
       sober_days = Person.calculate_sober_days(person)
@@ -53,7 +65,46 @@ defmodule XDaysSober.DailyEmailTest do
       assert email.subject == "One month sober 💥"
 
       assert email.text_body =~
-               "you did it! You are #{sober_days.days} days sober! Keep up the hard work and dedication to your sobriety, you are making positive changes in your life and deserve to be proud of yourself."
+               "I am proud of you for taking control of your recovery and making the choice to stay sober for the past month. This is a significant accomplishment, and you deserve to feel good about it!"
+    end
+
+    test "correct email for person after three month" do
+      one_month_before = Timex.now() |> Timex.shift(months: -3) |> Timex.to_date()
+      person = build(:person, sober_since: one_month_before)
+      sober_days = Person.calculate_sober_days(person)
+
+      email = DailyEmail.generate(person, sober_days)
+
+      assert email.subject == "3 months sober 💥"
+
+      assert email.text_body =~
+               "you did it! You are 3 months sober! Keep up the hard work and dedication to your sobriety, you are making positive changes in your life and deserve to be proud of yourself."
+    end
+
+    test "correct email for person after one year" do
+      one_year_before = Timex.now() |> Timex.shift(years: -1) |> Timex.to_date()
+      person = build(:person, sober_since: one_year_before)
+      sober_days = Person.calculate_sober_days(person)
+
+      email = DailyEmail.generate(person, sober_days)
+
+      assert email.subject == "One year sober 🚀🚀🚀"
+
+      assert email.text_body =~
+               "you did it! You are 1 year sober! Keep up the hard work and dedication to your sobriety, you are making positive changes in your life and deserve to be proud of yourself."
+    end
+
+    test "correct email for person after three year" do
+      one_year_before = Timex.now() |> Timex.shift(years: -3) |> Timex.to_date()
+      person = build(:person, sober_since: one_year_before)
+      sober_days = Person.calculate_sober_days(person)
+
+      email = DailyEmail.generate(person, sober_days)
+
+      assert email.subject == "3 years sober 🚀🚀🚀"
+
+      assert email.text_body =~
+               "you did it! You are 3 years sober! Keep up the hard work and dedication to your sobriety, you are making positive changes in your life and deserve to be proud of yourself."
     end
   end
 end
